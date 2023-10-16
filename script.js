0e267d3f145701ddbfbbd5d4383dbf44a325cbab
Vinffs <vin.demiranda@gmail.com>
2023-10-16 16:55:38 +0200

    exercise-prompt saved in array + for loop display

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+const shoppingList = new Array();
+const resultBox = document.querySelector('.alert');
 
+let stop = false;
+let i = 0;
+
+while (!stop) {
+  let question = prompt('Quali sono i prodotti da acquistare?')
+  if (question === 'stop') {
+    stop = true;
+  } else {
+    shoppingList.push(question);
+  }
+  i++;
+}
+
+for (let x = 0; x < shoppingList.length; x++) {
+  let result = document.createElement('div');
+  let product = shoppingList[x];
+  result.innerHTML = `${product}`;
+  resultBox.append(result);
+}
 
